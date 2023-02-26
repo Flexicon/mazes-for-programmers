e587bea06cdadd3bd331890e05074b28d6c2e974
Mike Repec <mikerepec@gmail.com>
2023-02-26 13:49:11 +0100

    Generate simple maze png

--- binary_tree_demo.rb
+++ binary_tree_demo.rb
@@ -5,3 +5,4 @@ grid = Grid.new(10, 10)
 BinaryTree.on(grid)
 
 puts grid
+grid.to_png(cell_size: 30).save('binary_demo.png')
--- sidewinder_demo.rb
+++ sidewinder_demo.rb
@@ -5,3 +5,4 @@ grid = Grid.new(10, 10)
 Sidewinder.on(grid)
 
 puts grid
+grid.to_png(cell_size: 30).save('sidewinder_demo.png')
